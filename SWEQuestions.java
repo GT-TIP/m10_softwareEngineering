7965cc75a240a6b8798b64eaf5879169a038d2c2
LirenFish <Monna@c-24-72-249-1.ni.gigamonster.net>
2016-11-20 23:33:52 -0500

    Practice Questions

---
+++ SWEQuestions.java
@@ -0,0 +1,18 @@
+/*
+Questions:
+1) Disign a Vending machine. A UML class diagram can be used to specify the domain model.
+2) Implement a CircularArray class that supports an array-like data structure which can be efficiently rotated. The class should use a generic type, and should support iteration via the standard for (obj 0: circularArray) notation
+
+3) Design a task scheduling algorithm for a web-server. When two tasks come to the server with the same timestamp. How would the server handle that? Hint: Extend abstract class and implement interfaces. Use polymorphism to initiate objects in correct forms. Overriding the atConflict method. Conflict handling should be different in different layers.  Be creative.
+	public interface Server{
+		private id;
+		public boolean connection(String user);
+	}
+	public abstract class taskForServer{
+		public abstract void register();
+	}
+}
+
+4) We have the following method used in a chess game: boolean canMoveto(int x, int y). This method is part of the Piece class and returns whether or not the piece can move to position (x,y). How you would test this method?
+
+*/
